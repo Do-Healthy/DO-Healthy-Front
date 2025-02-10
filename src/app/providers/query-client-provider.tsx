e622babe05c90d1f97b6e48301a269d6d2585fe2
clianor <ejrgus94160@gmail.com>
2025-02-10 23:43:11 +0900

    feat: Provider 컴포넌트로 앱 래핑

---
+++ src/app/providers/query-client-provider.tsx
@@ -0,0 +1,14 @@
+import { QueryClientProvider as TanStackQueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+
+import { queryClient } from '@/shared/libs/react-query';
+
+type QueryClientProviderProps = {
+  children: ReactNode;
+};
+
+export function QueryClientProvider(props: QueryClientProviderProps) {
+  const { children } = props;
+
+  return <TanStackQueryClientProvider client={queryClient}>{children}</TanStackQueryClientProvider>;
+}
