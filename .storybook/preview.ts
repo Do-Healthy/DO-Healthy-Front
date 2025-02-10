a2c10887731ad5ca5a7e8e46499c4aa977e84d34
clianor <ejrgus94160@gmail.com>
2025-02-10 23:30:05 +0900

    chore: msw 스토리북 설정 추가
    - msw-storybook-addon 추가
    - msw 설정 파일 추가
    - 스토리북 설정 파일 업데이트

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,10 @@
 import type { Preview } from '@storybook/react';
+import { initialize, mswLoader } from 'msw-storybook-addon';
 
 import '../pages/styles/globals.css';
+import { handlers } from './../src/__mocks__/handlers';
+
+initialize();
 
 const preview: Preview = {
   parameters: {
@@ -15,7 +19,11 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    msw: {
+      handlers
+    }
   },
+  loaders: [mswLoader],
 };
 
 export default preview;
