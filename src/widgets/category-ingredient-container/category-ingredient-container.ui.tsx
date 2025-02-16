4bedf3a3cecff6f26c37fac64472428da8282f38
soyeooooo <alththdus1@naver.com>
2025-02-16 21:31:53 +0900

    refactor : 함수명 수정
    ingredient category -> category

--- src/widgets/category-ingredient-container/category-ingredient-container.ui.tsx
+++ src/widgets/category-ingredient-container/category-ingredient-container.ui.tsx
@@ -1,29 +1,23 @@
 import { ComponentProps } from 'react';
 
-import { IngredientCategory } from '@/entities/ingredient.type';
+import { IngredientCategory } from '@/entities/ingredient/ingredient.type';
 import { IngredientCategoryCard } from '@/features/ingredient-category-card/ingredient-category-card.ui';
 
 import { INGREDIENT_CATEGORIES } from './category-ingredient-container.config';
 
-interface IngredientCategoryContainerProps extends ComponentProps<'div'> {
-  ingredientCategories?: readonly IngredientCategory[];
-}
+type CategoryContainerProps = ComponentProps<'div'> & {
+  categories?: readonly IngredientCategory[];
+};
 
 export function IngredientCategoryContainer({
-  ingredientCategories = INGREDIENT_CATEGORIES,
+  categories = INGREDIENT_CATEGORIES,
   ...props
-}: IngredientCategoryContainerProps) {
+}: CategoryContainerProps) {
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4" {...props}>
-      {ingredientCategories.map((ingredientCategory) => (
-        <div key={ingredientCategory.title} className="flex justify-center">
-          <IngredientCategoryCard
-            ingredientImageSrc={ingredientCategory.imageSrc}
-            ingredientIcon={ingredientCategory.icon}
-            ingredientTitle={ingredientCategory.title}
-            ingredientDescription={ingredientCategory.description}
-            className="w-full max-w-[326px]"
-          />
+      {categories.map((category) => (
+        <div key={category.title} className="flex justify-center">
+          <IngredientCategoryCard {...category} className="w-full max-w-[326px]" />
         </div>
       ))}
     </div>
