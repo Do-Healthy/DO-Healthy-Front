38e0af5057c8b769ddce3a7204abdf32a667d56e
soyeooooo <alththdus1@naver.com>
2025-02-28 03:02:56 +0900

    refactor : readonly 제거
    satisfies으로 타입 만족 확인

--- src/widgets/category-ingredient-container/category-ingredient-container.config.tsx
+++ src/widgets/category-ingredient-container/category-ingredient-container.config.tsx
@@ -1,7 +1,7 @@
 //이후 각 카드 경로 설정 예정
 import { IngredientCategory } from '@/entities/ingredient/ingredient.type';
 
-export const INGREDIENT_CATEGORIES: readonly IngredientCategory[] = [
+export const INGREDIENT_CATEGORIES = [
   {
     icon: '🌱',
     title: '두부/콩류',
@@ -27,4 +27,4 @@ export const INGREDIENT_CATEGORIES: readonly IngredientCategory[] = [
     imageSrc: 'https://picsum.photos/id/237/350/200',
     description: '두부, 렌틸콩, 병아리콩 등',
   },
-] as const;
+] satisfies IngredientCategory[];
